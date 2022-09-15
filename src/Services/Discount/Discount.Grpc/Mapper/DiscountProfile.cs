c6a3ea2fdc3f3431d74f31c0a1495b254700bdb3
Khasan <iut@DESKTOP-QKKG5S0>
2022-09-15 20:52:07 +0500

    Discount Grpc proto and serivces

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Discount.Grpc.Models;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>();
+            CreateMap<CouponModel, Coupon>();
+        }
+    }
+}
